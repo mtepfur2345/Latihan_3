f66d9d72ce1cdfc1bd34c1e9eb7a6a87533adf61
mtepfur2345 <muhamadtefurnur@gmail.com>
2024-11-25 20:10:46 +0700

    Add files via upload

---
+++ Latihan_3/variable.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Seleksi If-else</title>
+</head>
+<body>
+    
+<?php
+
+$a = 10;
+$b = 15;
+
+if ($a > $b) {
+    echo 'a lebih besar dari b';
+}
+else {
+    echo 'a tidak lebih besar dari b';
+}
+
+?>
+
+</body>
+</html>
